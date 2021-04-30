b878f4b5a74b67b45b2179fd7b8bbf2a1bf158a6
MaxenceJL <maxence.jacob-laforce@stanislas.qc.ca>
2021-04-30 16:25:07 -0400

    fichier.h de la commands/AdvanceUnderOneMeter.h, réalisé, fichier.cpp de la commands/AdvanceUnderOneMeter.cpp, en cours, prêt à être complété

---
+++ Activités/2020/Formation-Robot/Formation-API-Robot/src/main/cpp/commands/AdvanceUnderOneMeter.cpp
@@ -0,0 +1,23 @@
+#include <commands / AdvanceUnderOneMeter.h>
+#include <Robot.h>
+
+void AdvanceUnderOneMeter::AdvanceUnderOneMeter()
+{
+
+}
+
+void AdvanceUnderOneMeter::Initialize() {}
+
+void AdvanceUnderOneMeter::Execute()
+{
+    
+}
+
+void AdvanceUnderOneMeter::IsFinished()
+{
+    
+}
+
+void AdvanceUnderOneMeter::End() {}
+
+void AdvanceUnderOneMeter::Interrupted() {}
--- Activités/2020/Formation-Robot/Formation-API-Robot/src/main/include/commands/AdvanceUnderOneMeter.h
+++ Activités/2020/Formation-Robot/Formation-API-Robot/src/main/include/commands/AdvanceUnderOneMeter.h
@@ -1,2 +1,16 @@
+#pragma once
+#include <subsystems / TankDrive.h>
+#include <subsystems / UltrasonicSensor.h>
 
+class AdvanceUnderOneMeter : public frc::Command {
 
+public:
+
+AdvanceUnderOneMeter();
+void Initialize() override;
+void Execute() override;
+bool IsFinished() override;
+void End() override;
+void Interrupted() override;
+
+};
