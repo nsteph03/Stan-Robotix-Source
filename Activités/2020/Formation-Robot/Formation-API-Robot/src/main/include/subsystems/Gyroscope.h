e166043da38ecadf1d12519b5a396cbafd4f064d
antoineTrems <ta119786@stanislas.qc.ca>
2021-05-17 16:49:44 -0400

    subsystem gyroscope terminé git commit *git commit *git commit *git commit *! (normalement)

--- Activités/2020/Formation-Robot/Formation-API-Robot/src/main/include/subsystems/Gyroscope.h
+++ Activités/2020/Formation-Robot/Formation-API-Robot/src/main/include/subsystems/Gyroscope.h
@@ -16,15 +16,6 @@ public:
 
   void ResetSensors();
 
-  void Periodic() override;
-
-
-  /**
-   * Will be called periodically whenever the CommandScheduler runs during
-   * simulation.
-   */
-  void SimulationPeriodic() override;
-
 private:
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
